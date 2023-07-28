ed391fdf52c772f855a93e8947ab87c60eecbceb
baris.yildiz <baris.yildiz@portalgrup.com>
2023-07-28 11:28:18 +0300

    .

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,12 +6,19 @@ test('has title', async ({ page }) => {
   const homePage = new Homepage(page);
   await homePage.goTo();
   await expect(page).toHaveTitle("Ana Sayfa | Sunpet");
-  await homePage.goTo();  
+});
+
+test('can display stations', async ({ page }) => {
+  const homePage = new Homepage(page);
+  await homePage.goTo();
   await homePage.clickAcceptAll();
   await homePage.selectStateAndDistrict();
   await homePage.clickGoruntuleBtn();
   await expect(page).toHaveURL(/agri/);
- 
+
+  console.log(await page.locator("xpath = //div[@class='dealer-card']").count());
+  await expect(page.locator("xpath = //div[@class='dealer-card']")).toHaveCount(4);
+  await page.pause();
 });
 
 
